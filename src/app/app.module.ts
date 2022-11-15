8dd4ce5fd9f3d4fdf7f587559e11d4a6be09ef42
Ashwani Kumar <ashwani8785@gmail.com>
2022-11-15 07:37:07 +0530

    form OPI Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormEsopComponent } from './components/form-esop/form-esop.component';
 import { FormFcComponent } from './components/form-fc/form-fc.component';
 import { InstructionsComponent } from './Common/instructions/instructions.component';
+import { FormOpiComponent } from './components/form-opi/form-opi.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsModule} from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { InstructionsComponent } from './Common/instructions/instructions.compon
     FormEsopComponent,
     FormFcComponent,
     InstructionsComponent,
+    FormOpiComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +34,8 @@ import { InstructionsComponent } from './Common/instructions/instructions.compon
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/form-opi/form-opi.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-form-opi',
+  templateUrl: './form-opi.component.html',
+  styleUrls: ['./form-opi.component.css']
+})
+export class FormOpiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+ /*  ngAfterViewInit(){
+    console.log(this.tabset.tabs);
+  }
+
+  goto(id){
+    this.tabset.tabs[id].active = true;
+  } */
+}

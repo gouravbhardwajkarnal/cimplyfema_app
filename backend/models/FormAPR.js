2f459ca30d6e0a157531ea0035b3c99014186eaf
Gourav Bhardwaj <gouravbhardwajkarnal@outlook.com>
2022-11-28 18:40:18 +0530

    apr changes

--- backend/models/FormAPR.js
+++ backend/models/FormAPR.js
@@ -196,6 +196,28 @@ let FormAPR = new Schema({
    {
       type: Number
    },
+   ShareHoldingFE:[{
+      Person:{type:String},
+      Pstake:{type:Number},
+      ForeignPartner:{type:String},
+      FPstake:{type:Number},
+      Total: {type:Number},
+   }],
+   SDSDetails:[{
+       investment_SDS_Name: {type:String},
+    investment_SDS_Level: {type:String},
+    investment_SDS_Jurisdiction: {type:String},
+    investment_SDS_ParentName: {type:Number},
+    investment_SDS_ParentLevel: {type:String},
+    investment_SDS_ParentJurisdiction: {type:String},
+    investment_SDS_InvestmentAmount: {type:Number},
+    investment_SDS_InvestmentDate: {type:Date},
+    investment_SDS_LEI: {type:String},
+    investment_SDS_Type: {type:String},
+    investment_SDS_1987NIC: {type:String}, 
+    investment_SDS_2008NIC: {type:String},
+    investment_SDS_Stake:{type:Number},
+   }],
 
 },
    {

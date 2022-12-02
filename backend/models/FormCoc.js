93c11e1008038fc992adf87e349c2d069e7b82c1
Vipin Kumar <vipinkmr395@gmail.com>
2022-12-02 20:00:27 +0530

    coc change

--- backend/models/FormCoc.js
+++ backend/models/FormCoc.js
@@ -3,9 +3,33 @@ const Schema = mongoose.Schema;
 
 // Define collection and schema
 let FormCoc = new Schema({
+    COC_FDICIN:{type: number},
+    COC_FDI_CompanyName: { type: String},
+
+    FDI_FlatBuildingNumber: { type: String},
     COC_FDI_RBILetter:{type: Date},
+    COC_FDIIncorporationDate: { type: Date},
+    COC_FDIPanNo: { type: String},
+    COC_FDIFlatBuildingNumber:{type: Date},
+    COC_FDIFloorNumber: { type: String},
+    COC_FDIPremisesBuilding: { type: String},
+    COC_FDIRoadStreet:{type: Date},
+    COC_FDICity: { type: String},
+    COC_FDIState: { type: String},
+    COC_FDIPincode:{type: number},
+    COC_FDI_Email: { type: String},
+    COC_FDIMobile: { type: number},
+    COC_FDITelephone:{type: string},
+    COC_FDIFAX: { type: String},
+    COC_FDI_AuthPerson: { type: String},
+    COC_FDI_AuthPersonAddress:{type: string},
+    COC_FDI_AuthPAN: { type: String},
+    COC_FDI_AuthDesignation: { type: String}, 
     OPI_Sec_A_LEI: { type: String},
     OPI_Sec_A_PAN: { type: String},
+    OPI_Sec_A_LEI: { type: String},
+    OPI_Sec_A_PAN: { type: String},
+
     
 }, {
     collection: 'FormCoc'
---
+++ src/app/model/COCFdi.model.ts
@@ -0,0 +1,23 @@
+export class COC_FDI_DetailsList{     
+    COC_FDICIN:number;  
+    COC_FDI_CompanyName:string;  
+    COC_FDIIncorporationDate:Date;  
+    COC_FDIPanNo:string;
+    COC_FDIFlatBuildingNumber:string;
+    COC_FDIFloorNumber:number;
+    COC_FDIPremisesBuilding:number;
+    COC_FDIRoadStreet:string;
+    COC_FDICity:string;
+    COC_FDIState:string;
+    COC_FDIPincode:number;
+    COC_FDI_Email:string;
+    COC_FDIMobile:number;
+    COC_FDIFAX:string;
+    COC_FDITelephone:string;
+    COC_FDI_AuthPerson:string;
+    COC_FDI_AuthPersonAddress:string;
+    COC_FDI_AuthPAN:string;
+    COC_FDI_AuthDesignation:string;
+
+
+}

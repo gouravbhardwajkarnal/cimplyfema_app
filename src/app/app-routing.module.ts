9e8d39a6711f79ab9023338426779c6a49cea4e3
Gourav Bhardwaj <gouravbhardwajkarnal@outlook.com>
2022-12-12 12:29:22 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormFcComponent } from './components/ODI/form-fc/form-fc.component';
-import { FormEsopComponent } from './components/form-esop/form-esop.component';
-import { FormOpiComponent } from './components/form-opi/form-opi.component';
+import { FormEsopComponent } from './components/FDI/form-esop/form-esop.component';
+import { FormOpiComponent } from './components/ODI/form-opi/form-opi.component';
 import { FormAprComponent } from './components/ODI/form-apr/form-apr.component';
 import { OpiPdfComponent } from './components/ODI/opi-pdf/opi-pdf.component';
 import { FormCocComponent } from './components/COC/form-coc/form-coc.component';
@@ -10,9 +10,6 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  // { path: 'create-employee', component: EmployeeCreateComponent },
-  // { path: 'edit-employee/:id', component: EmployeeEditComponent },
-  // { path: 'employees-list', component: EmployeeListComponent },
   { path: 'form-fc',component: FormFcComponent},
   { path: 'form-esop', component: FormEsopComponent },
   { path: 'form-opi', component: FormOpiComponent },

05543b6ea9fa3c0ddf6a7bf008e7e3685bb5a813
Gourav Bhardwaj <gouravbhardwajkarnal@outlook.com>
2023-01-11 19:37:53 +0530

    design apr

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -11,8 +11,8 @@ import {
   providedIn: 'root',
 })
 export class ApiService {
-  baseUri: string = 'http://13.234.13.221:4000/api';
-  // baseUri: string = 'http://localhost:4000/api';
+  //baseUri: string = 'http://13.234.13.221:4000/api';
+  baseUri: string = 'http://localhost:4000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) {}
